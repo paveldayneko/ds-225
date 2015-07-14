0e77056e192d00005034f64393234c46de5eaa82
noogen <tommywhen@yahoo.com>
2015-07-14 09:21:04 -0500

    initial checkin

---
+++ README.md
@@ -0,0 +1,1 @@
+# Roundy's Corporate
---
+++ asset/225/storeApp.js
@@ -0,0 +1,22 @@
+﻿var storeApp = angular
+    .module('storeApp', ['infinite-scroll', 'ngRoute', 'ngSanitize', 'ngAnimate', 'ngTouch', 'chieffancypants.loadingBar', 'gsn.core', 'angulartics'])
+    .config(['$routeProvider', function ($routeProvider) {
+
+
+      //#region route config
+      var homeFile = gsn.getContentServiceUrl('/meta/' + gsn.config.ChainId + '/?name=home page&meta=home&type=text/html');
+      gsn.config.DefaultLayout = gsn.getContentServiceUrl('/meta/' + gsn.config.ChainId + '/?name=home page&meta=template&type=text/html');
+
+      $routeProvider
+          .when('/', {
+            templateUrl: homeFile,
+            caseInsensitiveMatch: true
+          })
+          .otherwise({
+            templateUrl: gsn.getContentUrl('/views/engine/static-content.html'),
+            caseInsensitiveMatch: true
+          });
+      //#endregion
+
+    }]);
+
